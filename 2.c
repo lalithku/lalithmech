6eb9214ad073f8c8ec69498a862c8121b2a6690b
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-21 16:27:12 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscrn();
+  printf(enter the a&b);
+  scanf("%d%d",&a,&b);
+ {
+ printf("odd");
+ }
+ else
+ {
+ printf("even");
+ }
+ getch()
