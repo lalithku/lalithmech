94f1f301617ef79c2d38228a8152818f3994b9c0
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-21 16:10:00 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,17 @@
+#inciude<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr()
+  printtf(enter the a&b);
+  scanf("%d%d",&a,&b);
+ {
+ printf("pasitive");
+ }
+ else
+ {
+ printf("nagative");
+ }
+ getch()
+}
