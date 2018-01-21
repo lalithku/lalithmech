ea628ff78229ca01f1f7ce3564b1b8620056c6fe
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-21 16:48:08 +0530

    Rename 2.c to 2.C

---
+++ 2.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscrn();
+  printf(enter the a&b);
+  scanf("%d%d",&a,&b);
+ {
+ printf("odd");
+ }
+ else
+ {
+ printf("even");
+ }
+ getch();
+ }
