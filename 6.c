48c05ceaa7189a2ad65459061140151e038832f8
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-27 09:35:22 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
-void main()
+int main()
 {
-    int a,b,c;
-    printf("enter the three numbers");
-    scanf("%d%d%d",&a,&b,&c);
-    if(a>c&&a>b)
-    printf("%d is greater number",a);
-    if(b>c&&b>a)
-    printf("%d is greater number",b);
-    else
-    printf("%d is greater number",c);
+int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if(a>b&&a>c)
+printf("%d",a);
+else 
+{
+if(b>c)
+printf("%d",b);
+else
+printf("%d",c);
+}
+return 0;
 }
