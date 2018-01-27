4a2f88ed91ce2e747a8252d1ee91fcc5a3c292b5
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-27 09:56:57 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#includ<conio.h>
+void main()
+{ 
+    int a,n;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    a=(n*(n+1))/2;
+    printf("%d",a);
+    getch();
+}
