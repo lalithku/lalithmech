42267c3d7825434a1793b9a129367c5e0e5d65db
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-27 09:37:51 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -2,17 +2,19 @@
 int main()
 {
 int a,b,c;
-scanf("%d",&a);
-scanf("%d",&b);
-scanf("%d",&c);
-if(a>b&&a>c)
-printf("%d",a);
-else 
-{
-if(b>c)
-printf("%d",b);
-else
-printf("%d",c);
-}
-return 0;
+printf("Enter three integers:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b && a>c)/*Series of if else statements to find the greatest of three numbers*/
+ {
+ printf("%d is the greatest number",a);
+ }
+else if(b>a && b>c)
+ {
+ printf("%d is the greatest number",b);
+ }
+else if(c>a && c>b)
+ {
+ printf("%d is the greatest number",c);
+ }
+return 0; 
 }
