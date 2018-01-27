be20ebd1d9ceef8ee06eb8c2566491a99e361231
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-27 09:30:51 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    char a;
+    printf("enter the input");
+    scanf("%c",&a);
+    if(a>='A'&&a<='z')
+    printf("the given input is alphabet");
+    else
+    printf("the given input is not alphabet");
+    
+}
+
