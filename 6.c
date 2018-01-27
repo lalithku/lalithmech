1377348c7186d7d91bb54bd7ac42b4d6e2813b49
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-27 09:44:44 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,20 +1,28 @@
 #include<stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
-int a,b,c;
-printf("Enter three integers:");
-scanf("%d %d %d",&a,&b,&c);
-if(a>b && a>c)/*Series of if else statements to find the greatest of three numbers*/
- {
- printf("%d is the greatest number",a);
- }
-else if(b>a && b>c)
- {
- printf("%d is the greatest number",b);
- }
-else if(c>a && c>b)
- {
- printf("%d is the greatest number",c);
- }
-return 0; 
+   int a ,b,c;
+   printf("enter the value");
+   scanf("%d %d %d",&a,&b,&c);
+   if(a>b && a>c)
+   {
+       printf("the larger value is %d",a);
+       
+   }
+    if(b>a && b>c)
+   {
+       printf("the larger value is %d,b");
+       
+   }
+  else
+  {
+      printf("the larger value is %d,c");
+      
+  }
+  else
+  {
+      printf("all are equal")
+  }
+   getch();
 }
