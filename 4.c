37d64928f52b14ed2036cef7b03489f304ff4660
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-27 09:28:24 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char h[6]={'a','e','i','o','u'};
+char g;
+scanf("%c",&g);
+if(h[0]==g||h[1]==g||h[2]==g||h[3]==g||h[4]==g)
+printf("\n the given alphabet is vowel");
+else
+printf("\n the given alphbet is consonant");
+}
+© 2018 GitHub, 
