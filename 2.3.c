3c7af03fc0face8d7a65417b159b8d15b80d1569
lalithku <35654373+lalithku@users.noreply.github.com>
2018-01-27 10:49:28 +0530

    Create 2.3.c

---
+++ 2.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n=6,t=0,i=0;
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+t=t+1;
+break;
+}
+if(t==1)
+printf("it is not a prime number");
+else
+printf("it is not a prime number");
+getch();
+}
