549e2935d387b5c6c15a321718ae3546eccf6c22
lalithku <35654373+lalithku@users.noreply.github.com>
2018-02-08 16:54:12 +0530

    Create 4.3.c

---
+++ 4.3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,c=0;
+clrscr();
+char a[50]=("hi ma what about ur studies");
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+c++;
+}
+printf("%d",c);
+}
+getch();
+}
