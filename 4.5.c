4986297afd8df541505f1a63985d4dade334f06c
lalithku <35654373+lalithku@users.noreply.github.com>
2018-02-13 16:17:28 +0530

    Create 4.5.c

---
+++ 4.5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+void main()
+{
+	char a[50];
+	int n,i,count=0;
+	printf("ENTER THE INPUT SENTENCE:\n");
+	gets(a);
+	n=strlen(a);
+	printf("\nTHE LENGTH OF SENTENCE IS %d",n);
+	for(i=0;i<n;i++)
+	{
+		if(isdigit(a[i]))
+		{
+			count++;
+		}
+	}
+	printf("\nTHE NO.OF.INTEGERS IN THE GIVEN SENTENCE ARE %d",count);
+}
