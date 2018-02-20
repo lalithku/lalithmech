77154af7298b9e0b67aee41820ffb7665dcf7e5a
lalithku <35654373+lalithku@users.noreply.github.com>
2018-02-20 16:54:54 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int n,next;
+printf("to print the next number of the given number\n");
+scanf("%d",&n);
+next=n+1;
+printf("the next number of the given number is %d\n",next);
+}
