420431ca89a8ac177e8a4eb3aa342b7bdac1bafb
lalithku <35654373+lalithku@users.noreply.github.com>
2018-02-20 15:57:37 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	char ch[50];
+	int n,i;
+	printf("ENTER THE INPUT STRING:\n");
+	scanf("%s",ch);
+	printf("\nENTER THE NO.OF.TIMES TO PRINT:\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("\n%s",ch);
+	}
+	}
