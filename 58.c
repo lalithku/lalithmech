1a2d807933c41024eee79fda837519b075c7b691
lalithku <35654373+lalithku@users.noreply.github.com>
2018-03-15 16:04:41 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,t;
+	printf("ENTER THE INPUT NOS\n");
+	scanf("%d%d",&a,&b);
+	printf("THE NOS BEFORE SWAPPING:\n%d\n%d\n",a,b);
+	t=a;
+	a=b;
+	b=t;
+	printf("\nTHE NOS AFTER SWAPPING:\n%d\n%d\n",a,b);
+}
